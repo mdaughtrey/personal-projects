bbdb0babeece2376005dc6553a27014813db42b8
Matthew Daughtrey <matt@daughtrey.com>
2014-07-07 00:15:51 -0400

    xxx

--- arduino/projv3/projector.cpp
+++ arduino/projv3/projector.cpp
@@ -3,49 +3,112 @@
 
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 
-#define KBDDATA 12
-#define KBDCLOCK 11
+#define KBDDATA 7
+#define KBDCLOCK 6
+#define LED 13
 
-PS2Mouse mouse(KBDCLOCK, KBDDATA, STREAM);
+long xx = 0;
+long yy = 0;
+unsigned long timer = 0;
 
-
-        //Serial.println("? - this text");
-
-//void toMouse(unsigned char byte)
-//{
-//    Serial.print("Writing: ");
-//    Serial.print(byte, 16);
-//    Mouse.write(byte);
-//    Serial.println("");
-//}
-//
-//unsigned char fromMouse()
-//{
-//    Serial.print("Reading: ");
-//    unsigned char byte = Mouse.read();
-//    Serial.print(byte, 16);
-//    Serial.println("");
-//}
+PS2Mouse mouse;
 
 void setup ()
 {
-//    pinMode(lamp, OUTPUT);
-
- //   digitalWrite(sensor, HIGH);
+//    digitalWrite(3, HIGH);
+//    pinMode(3, OUTPUT);
+//    digitalWrite(3, HIGH);
     Serial.begin(57600);
-    Serial.print("init");
-    mouse.initialize(); 
-    Serial.print("init done");
+    Serial.println("OK");
+    timer = millis();
+    mouse.initialize();
+    mouse.set_remote_mode();
 }
+
+#if 0
+void rawReport(unsigned char (&data)[3])
+{
+    mouse.rawReport(data);
+    if (data[0])
+    {
+        if (data[0] & (1 << 4))
+        {
+            xx -= ((data[1] ^ 0xff) + 1);
+        }
+        else
+        {
+            xx += data[1];
+        }
+        if (data[0] & (1 << 5))
+        {
+            yy -= ((data[2] ^ 0xff) + 1);
+        }
+        else
+        {
+            yy += data[2];
+        }
+    }
+}
+#endif // 0
 
 void loop ()
 {
-    int data[2];
-    mouse.report(data);
-    Serial.print(data[0]); // Status Byte
-    Serial.print(":");
-    Serial.print(data[1]); // X Movement Data
-    Serial.print(",");
-    Serial.print(data[2]); // Y Movement Data
-    Serial.println();
+    int data[3] = {0, 0, 0};
+    int * pData = &data[0];
+
+//    mouse.report(pData);
+//    xx += data[1];
+//    yy += data[2];
+//
+//    if ((millis() - timer) > 1000)
+//    {
+//        Serial.print(data[0], 16);
+//        Serial.print(' ');
+//        Serial.print(data[1]);
+//        Serial.print(' ');
+//        Serial.print(data[2]);
+//        Serial.print(' ');
+//        Serial.print(xx);
+//        Serial.print(' ');
+//        Serial.print(yy);
+//        Serial.println(" ");
+//        timer = millis();
+//    }
+
+    if (Serial.available())
+    {
+        switch (Serial.read())
+        {
+         case '0':
+             xx = 0;
+             yy = 0;
+             break;
+
+         case 'm':
+             mouse.set_remote_mode();
+             break;
+
+         case 'x': // reset
+             Serial.print("Init");
+             mouse.initialize();
+             Serial.println('.');
+             break;
+
+         case 'r': // raw report
+             mouse.report(pData);
+             xx += data[1];
+             yy += data[2];
+             Serial.print(data[0], 16);
+             Serial.print(' ');
+             Serial.print(data[1]);
+             Serial.print(' ');
+             Serial.print(data[2]);
+             Serial.print(' ');
+             Serial.print(xx);
+             Serial.print(' ');
+             Serial.print(yy);
+             Serial.println(" ");
+             break;
+        }
+    }
 }
