fb38bcb31aa7419b4ef3eeca9544c8b97215927b
Matt <mattd@Matts-MacBook-Pro.local>
2016-04-12 01:17:12 -0400

    ttt

--- arduino/projv3/scripts/run.sh
+++ arduino/projv3/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./runproj.py --startframe 0 --endframe 1000 --cameradir /Volumes/192.168.0.222/DCIM \
+./runproj.py --startframe 0 --endframe 1000 \
     --targetdir ./tmp --pretension 45 --mode super8 --length large
 # /Volumes/imageinput/Niall/NK0032
