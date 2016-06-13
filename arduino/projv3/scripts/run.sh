c6830b5e34253704e9b5c1b75909ae44fac1e505
Matt <mattd@Matts-MacBook-Pro.local>
2016-06-13 00:30:43 -0400

    runproj stuff

--- arduino/projv3/scripts/run.sh
+++ arduino/projv3/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./runproj.py --startframe 0 --endframe 1000 \
-    --targetdir ./tmp --pretension 45 --mode super8 --length large
+./runproj.py --numframes 100 \
+    --targetdir ./tmp --pretension 25 --mode 8mm 
 # /Volumes/imageinput/Niall/NK0032
