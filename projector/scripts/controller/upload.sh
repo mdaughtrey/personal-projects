5bb2497a9cdcdae75d2d64c6e7786fca92c61673
mattd <mattd@vproj>
2016-08-31 01:00:44 -0400

    eee

--- projector/scripts/controller/upload.sh
+++ projector/scripts/controller/upload.sh
@@ -2,12 +2,13 @@
 
 set -o xtrace
 
-SRCDIR=~/Documents/vmshared/scans/Niall/NK0032/100PHOTO/
+SRCDIR=/media/sf_vmshared/scans/Niall/NK0032/100PHOTO/
 
 container=$(basename $SRCDIR)
 for filename in ${SRCDIR}/*.JPG
 do
     basefile=$(basename $filename)
     curl -i  -F "filename=@${filename}" http://192.168.0.23:5000/upload\?project=uploadtest\&container=${container}\&filename=${basefile}
+    exit 0
 done
 
