aa66107b82fedb21823aedbfa77bd7f42ef5e8f9
mattd <mattd@vproj>
2016-09-09 01:28:11 -0400

    rrr

---
+++ projector/scripts/run_gentitle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash -x ./gentitle.sh -t /media/sf_vmshared/scans/uploadtest \
+    -i /media/sf_vmshared/scans/uploadtest/100/fused \
+    -o /media/sf_vmshared/scans/uploadtest/titles
