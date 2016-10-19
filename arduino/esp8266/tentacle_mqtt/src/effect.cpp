6eb87721cfef012c49ec0906a5c1b22a9c4576de
mdaughtrey <matt@daughtrey.com>
2016-10-18 22:18:45 -0400

    skp

--- arduino/esp8266/tentacle_mqtt/src/effect.cpp
+++ arduino/esp8266/tentacle_mqtt/src/effect.cpp
@@ -1,31 +1,102 @@
 #include <effect.h>
 
+#define LED_HEARTBEAT 2
+
+
+uint8_t gammaTable[16] = {
+    0, 1, 2, 6, 12, 20, 31, 44, 60, 79, 100, 125, 153, 183, 218, 255
+};
+
 void
 Effect::effectChase()
 {
-    m_pixels.setPixelColor(m_effectCount, 0, 0, 0);
+    m_pixels.setPixelColor(m_effectCount,
+        std::get<0>(m_background),
+        std::get<1>(m_background),
+        std::get<2>(m_background));
+
+    //m_pixels.show();
     m_effectCount++;
     m_effectCount %= m_numPixels;
+
     m_pixels.setPixelColor(m_effectCount,
         std::get<0>(m_color),
         std::get<1>(m_color),
         std::get<2>(m_color));
+
+    m_pixels.show();
+//    delay(50);
+}
+
+void
+Effect::effectFlash()
+{
+    if (m_effectCount % 2)
+    {
+        setAll(m_color);
+    }
+    else
+    {
+        setAll(m_background);
+    }
+    m_effectCount++;
+}
+
+void
+Effect::effectAll()
+{
+    setAll(m_color);
+}
+
+void
+Effect::effectHeartbeat()
+{
+    int duty = int(sin(radians(m_effectCount)) * 15);
+    if (duty > 0)
+    {
+        analogWrite(LED_HEARTBEAT, 1023 - gammaTable[duty] * 4);
+        uint16_t dimmer(255 / gammaTable[duty]);
+        RGBColor color(
+            std::get<0>(m_color) / dimmer,
+            std::get<1>(m_color) / dimmer,
+            std::get<2>(m_color) / dimmer);
+        setAll(color);
+    }
+    m_effectCount += 10;
+    m_effectCount %= 360;
+}
+
+void
+Effect::setAll(RGBColor & color)
+{
+    for (uint8_t ii = 0; ii < m_pixels.numPixels(); ii++)
+    {
+        m_pixels.setPixelColor(ii,
+            std::get<0>(color),
+            std::get<1>(color),
+            std::get<2>(color));
+    }
     m_pixels.show();
 }
 
 void
 Effect::effectTrail()
 {
-    m_pixels.setPixelColor(m_effectCount, 0, 0, 0);
-    m_effectCount++;
-    m_effectCount %= m_numPixels;
-    for (uint8_t ii = 0; ii < 4; ii++)
+    uint8_t ii;
+    for (ii = 0; ii < 4; ii++)
     {
         m_pixels.setPixelColor((m_effectCount + (ii * (m_numPixels - 1))) % m_numPixels,
             std::get<0>(m_color) >> ii,
             std::get<1>(m_color) >> ii,
             std::get<2>(m_color) >> ii);
     }
+    m_pixels.setPixelColor((m_effectCount + (ii * (m_numPixels - 1))) % m_numPixels,
+        std::get<0>(m_background),
+        std::get<1>(m_background),
+        std::get<2>(m_background));
+
+    m_effectCount++;
+    m_effectCount %= m_numPixels;
     m_pixels.show();
 }
 
@@ -53,6 +124,12 @@ void
 Effect::color(uint8_t red, uint8_t green, uint8_t blue)
 {
     m_color = std::make_tuple(red, green, blue);
+}
+
+void
+Effect::background(uint8_t red, uint8_t green, uint8_t blue)
+{
+    m_background = std::make_tuple(red, green, blue);
 }
 
 void
--- arduino/esp8266/tentacle_mqtt/src/effect.h
+++ arduino/esp8266/tentacle_mqtt/src/effect.h
@@ -5,21 +5,31 @@
 class Effect
 {
 protected:
+    typedef std::tuple<uint8_t, uint8_t, uint8_t> RGBColor;
     Adafruit_NeoPixel & m_pixels;
     void effectChase();
     void effectNone() {};
     void effectOff();
     void effectTrail();
+    void effectAll();
+    void effectFlash();
+    void effectHeartbeat();
     typedef void (Effect::*memfun)();
+
+    void setAll(RGBColor & color);
     std::map<std::string, memfun> m_effects =
     {
         { "off", &Effect::effectOff },
         { "none", &Effect::effectNone },
         { "chase", &Effect::effectChase },
-        { "trail", &Effect::effectTrail }
+        { "all", &Effect::effectAll },
+        { "flash", &Effect::effectFlash },
+        { "trail", &Effect::effectTrail },
+        { "heartbeat", &Effect::effectHeartbeat }
     };
     memfun m_currentEffect = &Effect::effectNone;
-    std::tuple<uint8_t, uint8_t, uint8_t> m_color { 0, 0, 0 };
+    RGBColor m_color { 0, 0, 0 };
+    RGBColor m_background { 0, 0, 0 };
     uint16_t m_period;
     uint32_t m_lastUpdate;
     uint16_t m_effectCount;
@@ -39,4 +49,5 @@ public:
     void select(const char * type);
     void loop();
     void color(uint8_t red, uint8_t green, uint8_t blue);
+    void background(uint8_t red, uint8_t green, uint8_t blue);
 };
