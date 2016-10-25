ccc5a7b9c84bfcafb1359044d31fe882fffc94b7
mattd <mattd@vproj>
2016-10-25 16:53:32 -0400

    dd

--- projector/scripts/controller/nx300.py
+++ projector/scripts/controller/nx300.py
@@ -1,7 +1,11 @@
+from multiprocessing import Process
+import threading
+import subprocess
+
 class NX300():
     AIRPORT='en0'
-    SSID='CamSD'
-    KEY='12345678'
+    SSID='AP_SSC_NX300-0-0D:D0:A1'
+    KEY=''
     IP='192.168.0.222'
     SETAIRPORTPOWER='networksetup -setairportpower '
     GETAIRPORTPOWER='networksetup -getairportpower '
@@ -14,16 +18,25 @@ class NX300():
     def connect(self):
         if False == self._isAirportOn():
             self._turnAirportOn()
-        while False == self.connectToCard():
+        while False == self.remoteConnect():
             pass
-
 
     def disconnect(self):
         self._turnAirportOff()
 
+    def remoteConnect(self):
+        logger.info("remoteConnect")
+        result = subprocess.check_output((NX300.SETAIRPORTNETWORK + "%s %s %s" % (options.wifidev, NX300.SSID, NX300.KEY)).split(' '))
+        logger.debug(result)
+        if '' != result:
+            logger.error("Connect to %s failed: %s" % (NX300.SSID, result))
+            raise RuntimeError("restart")
+
+        logger.info("WIFI Connected to %s" % NX300.SSID)
+
     def _isAirportOn(self):
         try:
-            result = subprocess.check_output((FlashAir.GETAIRPORTPOWER + "%s" % FlashAir.AIRPORT).split(' '))
+            result = subprocess.check_output((NX300.GETAIRPORTPOWER + "%s" % NX300.AIRPORT).split(' '))
             result = result.split(':')[-1].strip()
             if "On" == result:
                 return True
@@ -34,8 +47,8 @@ class NX300():
             return False
 
     def _turnAirportOn(self):
-        rc = subprocess.call((FlashAir.SETAIRPORTPOWER + "%s on" % FlashAir.AIRPORT).split(' '))
+        rc = subprocess.call((NX300.SETAIRPORTPOWER + "%s on" % NX300.AIRPORT).split(' '))
 
     def _turnAirportOff(self):
-        rc = subprocess.call((FlashAir.SETAIRPORTPOWER + "%s off" % FlashAir.AIRPORT).split(' '))
+        rc = subprocess.call((NX300.SETAIRPORTPOWER + "%s off" % NX300.AIRPORT).split(' '))
 
