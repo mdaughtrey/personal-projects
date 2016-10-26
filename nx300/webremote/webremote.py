c4b3be249e18c4fdf3a5be702172142f6de66377
mattd <mattd@vproj>
2016-10-25 20:05:50 -0400

    ddd

--- nx300/webremote/webremote.py
+++ nx300/webremote/webremote.py
@@ -10,10 +10,27 @@ args = argparser.parse_args()
 
 
 def init():
-    conn = httplib.HTTPConnection('192.168.107.1/mode/control')
-    conn 
+    conn = httplib.HTTPConnection('192.168.107.1', 7788)
+    headers = {
+        'User-Agent': 'SEC_MODE_+1231231234',
+        'Connection': 'Close',
+        'NTS': 'Alive',
+        'HOST-Mac': '02:00:00:00:00:00',
+        'HOST-Address': '192.168.0.18',
+        'HOST-port': '7788',
+        'HOST-PNumber': '+1231231234',
+        'Access-Method': 'Manual',
+        'CALLBACK': '<http://192.168.0.18:7792/eventCallback>'
+    }
+    conn.request('HEAD', '/mode/control', None, headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+
+    data = response.read()
+    print data
+    conn.close()
 
 def main():
-    pass
+    init()
 
 main()
