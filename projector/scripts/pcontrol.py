24ed37ed1c1da019dec5f024024ca1eb7ae57cd4
mdaughtrey <matt@daughtrey.com>
2016-12-25 19:59:13 -0500

    pcontrol.py

--- projector/scripts/pcontrol.py
+++ projector/scripts/pcontrol.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import time
+import io
 import sys
 import pdb
 import logging
@@ -11,10 +12,12 @@ import argparse
 import serial
 import BeautifulSoup
 import telnetlib
+import socket
 
 SerialPort = '/dev/tty.usbserial-A601KW2O'
-CameraIP = ''
+CameraIP = '192.168.0.33'
 CameraPort = 1000
+TelnetRoot = '/mnt/mmc'
 
 logging.basicConfig(level = logging.DEBUG, format='%(asctime)s %(levelname)s %(lineno)s %(message)s')
 logger = logging.getLogger('pcontrol')
@@ -25,7 +28,7 @@ logger.addHandler(fileHandler)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--simulate', action='store_true', help='simulate (no I/O)')
-parser.add_argument('--cycles', dest='cycles', help='number of frames to capture')
+parser.add_argument('--cycles', dest='cycles', default=10, type=int, help='number of frames to capture')
 parser.add_argument('--filmtype', dest='filmtype', help='film type (8mm/super8)')
 
 args = parser.parse_args()
@@ -37,23 +40,33 @@ def cameraOn():
 def triple():
     pass
 
-def click(http):
-    def subclick(http, cmds):
+def click(cb):
+    def subclick(cmds):
         for cmd in cmds:
-            url = '/api/v1/input/inject?key' % cmd
-            http.request('GET', url)
+#            pdb.set_trace()
+            url = '/api/v1/input/inject?key%s' % cmd
+            while True:
+                try:
+                    http = httplib.HTTPConnection(CameraIP, 80, timeout = 5)
+                    http.request('GET', url)
+                    break
+                except socket.error as ee:
+                    logger.debug("No connection, retry in 1 sec")
+                    time.sleep(5)
+
             response = http.getresponse()
             data = response.read()
 
-    subclick(http, ['down=Super_L','down=Super_R'])
-    time.sleep(1)
-    subclick(http, ['up=Super_L','up=Super_R'])
+    cb()
+    subclick(['down=Super_L','down=Super_R'])
+    #time.sleep(1)
+    subclick(['up=Super_L','up=Super_R'])
 
 def armLamp():
     pass
 
-def transferPicture(dir, filename, telnet, http):
-#    cameraConn = httplib.HTTPConnection(CameraIP, 80)
+def transferPicture(dir, filename, telnet):
+    http = httplib.HTTPConnection(CameraIP, 80)
     fileurl = '/DCIM/%s/%s' % (dir, filename)
     fileurl = fileurl.encode('ascii', 'ignore')
     http.request('GET', fileurl)
@@ -72,19 +85,31 @@ def transferPicture(dir, filename, telnet, http):
     logger.debug('transferPicture end')
 
 
-def transferPictures(http):
+def transferPictures(telnet):
+    logger.debug('transferPictures')
     numAvailable = 0
-    while numAvailable < 3:
-        telnet = telnetlib.Telnet(CameraIP)
-        telnet.read_until('nx300:/#')
+    while numAvailable < 1:
         # wait for at least 3 available images
         telnet.write("find %s/DCIM -name '*.JPG' | wc -l\n" % TelnetRoot)
-        tData = telnet.read_until('nx300:/#').replace('\r\n', '\n')
-        numAvailable = int(tData.split('\n')[-2])
-        logger.debug('numAvailable %u' % numAvailable)
-        time.sleep(1)
+#        pdb.set_trace()
+        tData = ''
+        while True:
+            rx = telnet.read_very_eager()
+            if '' == rx:
+                break
+            time.sleep(0.5)
+            tData += rx
+        logger.debug(tData)
 
-#     conn = httplib.HTTPConnection(CameraIP, 80)
+        for elem in tData.split('\r\n'):
+            try:
+                numAvailable = int(elem)
+            except:
+                pass
+        logger.debug('numAvailable %u' % numAvailable)
+        time.sleep(0.1)
+
+    http = httplib.HTTPConnection(CameraIP, 80)
 #     headers = {
 #     'Host': CameraIP,
 #     'Connection': 'keep-alive',
@@ -106,7 +131,7 @@ def transferPictures(http):
     bs=BeautifulSoup.BeautifulSoup(data)
     dirtags=[ee.string for ee in bs.findAll('a') if 'PHOTO' in ee.string]
     for dir in dirtags:
-#        conn = httplib.HTTPConnection(CameraIP, 80)
+        http = httplib.HTTPConnection(CameraIP, 80)
         http.request('GET', '/DCIM/%s' % dir)
         response = http.getresponse()
         fileInfo = response.read()
@@ -114,36 +139,64 @@ def transferPictures(http):
         fileTags=[ee.string for ee in bs.findAll('a') if '.JPG' in ee.string]
 #        conn.close()
         for file in fileTags:
-            transferPicture(dir, file, telnet, http)
+            transferPicture(dir, file, telnet)
 #    conn.close()
 
-def doCycles(serial, http):
-    serial.write('2m-100a-')
-    click(http)
-    serial.write('10m-100a-')
-    click(http)
-    serial.write('30m-100a-')
-    click(http)
-    time.sleep(2)
-    transferPictures(http)
+def doCycles(serial, telnet):
+    def waitCardOps(telnet):
+        logger.debug("Waiting on card ops")
+        telnet.write('/mnt/mmc/moncard.sh\n')
+        rx = telnet.read_until("Timeout")
+        logger.debug("moncard.sh returns %s" % rx)
+        if "Wait " in rx:
+            logger.error("Error waiting on SD card activity")
+
+    for ii in xrange(1, args.cycles):
+        logger.debug("Frame %u of %u" % (ii, args.cycles))
+        logger.debug("Click 1")
+        click(lambda: serial.write('2m-200a-'))
+#        waitFor(serial, '{LampDone}')
+        waitCardOps(telnet)
+        transferPictures(telnet)
+        logger.debug("Click 2")
+        click(lambda: serial.write('10m-200a-'))
+#//        waitFor(serial, '{LampDone}')
+        waitCardOps(telnet)
+        transferPictures(telnet)
+        logger.debug("Click 3")
+        click(lambda: serial.write('30m-200a-'))
+#//        waitFor(serial, '{LampDone}')
+        waitCardOps(telnet)
+        transferPictures(telnet)
+
+def waitFor(port, text):
+    accum = ''
+    logger.debug("Waiting on %s" % text)
+    while not text in accum:
+        accum += port.read()
+#        logger.debug('accum %s' % accum)
+    logger.debug("Received %s" % text)
+    return accum
 
 def main():
-    pdb.set_trace()
     logger.debug("Init")
     serPort = serial.Serial(SerialPort, 57600)
     logger.debug("Opening %s" % SerialPort)
     if serPort.isOpen():
         serPort.close()
     serPort.open()
-    serPort.write(b'cvd') # on, verbose, 8mm
+    waitFor(serPort, '{State:Ready}')
+    serPort.write(b'cd') # on, verbose, 8mm
+    waitFor(serPort, '{mode:8mm}')
 
-    http = httplib.HTTPConnection(CameraIP, 80, timeout = 5)
     if False == args.simulate:
         time.sleep(2)
         if False == serPort.isOpen():
             logger.error("Cannot open %s, exiting" % SerialPort)
             return 1
-        doCycles(serPort, http)
+        telnet = telnetlib.Telnet(CameraIP)
+        telnet.read_until('nx300:/#')
+        doCycles(serPort, telnet)
 
     logger.debug("Closing %s" % SerialPort)
     serPort.write(b'C')
