9ea7432d0fd1e104e2667cf41ea1725a2ad58c0e
mattd <mattd@vproj>
2017-01-08 17:16:08 -0500

    gentitle

--- projector/scripts/test.sh
+++ projector/scripts/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-case "$1" in
-    one) echo one ;;
-    two|three) echo something else;;
-esac
+FONT="/usr/share/fonts/truetype/droid/DroidSerif-BoldItalic.ttf"
+SEM="sem --will-cite"
+convert -background transparent -stroke yellow -strokewidth 2 \
+-fill blue -font $FONT -size x200 \
+'label:Sample Title' \
+/media/sf_vmshared/scans//blister/title/titleline_0.png
