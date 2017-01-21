e10b2395c581a27e0bb0b39e65d028ee7ffdc284
mdaughtrey <matt@daughtrey.com>
2017-01-21 16:38:44 -0500

    tyu

--- projector/scripts/runp.sh
+++ projector/scripts/runp.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-
-cycles=200
-
-while [[ "$cycles" != "0" ]]
-do
-    ./pcontrol.py --filmtype s8 --mode pipeline --cycles 100
-    if [[ "$?" != "0" ]]
-    then
-        echo Something crapped out
-        exit 0
-    fi
-    ((cycles-=100))
-done
-echo Done
-exit 0
+./pcontrol.py --filmtype s8 --mode pipeline --cycles 600
