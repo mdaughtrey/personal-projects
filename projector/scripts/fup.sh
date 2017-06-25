16791bf3ca21898c1ec5cde7114ceed834b49561
mdaughtrey <matt@daughtrey.com>
2017-06-25 17:15:06 -0400

    update from pi

--- projector/scripts/fup.sh
+++ projector/scripts/fup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+CONTROLLER=192.168.0.42
+
 #./fileuploader.py --ip 192.168.0.41 --port 5000 --dir /home/mattd/cam/images
-./fileuploader.py --ip 192.168.0.31 --port 5000 --dir /tmp
+./fileuploader.py --ip $CONTROLLER --port 5000 --dir /tmp
---
+++ projector/scripts/singen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import math
+import pdb
+import sys
+
+TENSION_START = 40
+TENSION_END = 20
+FRAMES = 3500
+
+tensions = []
+for frame in xrange(0, FRAMES):
+    range = TENSION_START - TENSION_END
+    angle = math.radians((90*frame/FRAMES)+90)
+    sine = math.sin(angle)
+    tension = (sine*(TENSION_START-TENSION_END))+TENSION_END
+    tensions.append(int(math.floor(tension)))
+
+for val, count in [(elem, tensions.count(elem)) for elem in sorted(set(tensions), reverse=True)]:
+    print "%s %s" % (val, count)
+    #sys.stdout.write("%s %s " % (val, count))
