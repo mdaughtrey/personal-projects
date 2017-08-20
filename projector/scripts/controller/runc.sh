2b6be146095bfecc3484e3122f3af8c0bd0d4c28
mdaughtrey <matt@daughtrey.com>
2017-08-19 22:24:40 -0400

    aaa

--- projector/scripts/controller/runc.sh
+++ projector/scripts/controller/runc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROJECT=005
+PROJECT=007
 
 
 #./controller.py --jobmode proc --project blister --film super8 #--worker pc --worker ac  # --worker tf --worker gc
-./controller.py --jobmode proc --project $PROJECT --film super8
+./controller.py --jobmode proc --project $PROJECT --film 8mm
