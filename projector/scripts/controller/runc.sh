3b920f04e8a82a5383ec7f26970ac72d2e9d12b1
mdaughtrey <matt@daughtrey.com>
2017-08-24 23:20:10 -0400

    no precrop, fix ac3

--- projector/scripts/controller/runc.sh
+++ projector/scripts/controller/runc.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 PROJECT=007
-
-
-#./controller.py --jobmode proc --project blister --film super8 #--worker pc --worker ac  # --worker tf --worker gc
-=======
-PROJECT=006
-
 
 #./controller.py --jobmode proc --project blister --film super8 #--worker pc --worker ac  # --worker tf --worker gc
 #./controller.py --jobmode proc --project $PROJECT --film super8
->>>>>>> 3bcc944b7c3db399283b76cd642c7705b0192fec
-./controller.py --jobmode proc --project $PROJECT --film 8mm
+./controller.py --jobmode proc --project $PROJECT --film super8
