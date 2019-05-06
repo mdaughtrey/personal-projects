a39dd8fe769401d519b9ff06b1f3417633c3c1ed
mdaughtrey <matt@daughtrey.com>
2019-05-06 04:00:44 +0000

    dev

--- projector/blackadj/blackadj.py
+++ projector/blackadj/blackadj.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import numpy
+import cv2
 import imageio
 from PIL import Image
 import pdb
 
-uncorr = imageio.imread("./test.ppm")
+uncorr = cv2.imread("./test.ppm")
+pdb.set_trace()
 imageio.imwrite("test.png", uncorr)
