2243f675d3dc9f853ff11a933dd169cc0facc357
mdaughtrey <matt@daughtrey.com>
2019-07-16 20:48:21 -0400

    rawcap/fup

--- projector/scripts/rawcap.sh
+++ projector/scripts/rawcap.sh
@@ -4,4 +4,4 @@ pushd /home/mattd/personal-projects/projector/raspiraw
 camera_i2c
 popd
 
-./rawcap.py --film=8mm --frame=3500 --dev=/mnt/exthd --project 8mm2 $@
+./rawcap.py --film=8mm --frame=3500 --dev=/mnt/exthd --dir /mnt/exthd/8mm2 $@
