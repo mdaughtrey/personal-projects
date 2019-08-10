9a79a12065ec8f5852c558221f4354047b67e8ae
mdaughtrey <matt@daughtrey.com>
2019-08-09 20:43:24 -0400

    fup

--- projector/scripts/fup.sh
+++ projector/scripts/fup.sh
@@ -5,4 +5,4 @@ CONTROLLER=DESKTOP-TOJK787
 
 #./fileuploader.py --ip $CONTROLLER --port 5000 --dir /home/mattd/capture --nodelete --frames=3
 #./fileuploader.py --ip $CONTROLLER --port 5000 --dir /home/mattd/capture 
-./fileuploader.py --ip $CONTROLLER --port 5000 --dir /mnt/exthd/nofilm
+./fileuploader.py --ip $CONTROLLER --port 5000 --dir /mnt/exthd/fm101
