4ff3635b79def26348cdaab69a4cd7e7146a9ce9
mdaughtrey <mdaughtrey@gmail.com>
2019-08-25 21:35:43 -0400

    pio

---
+++ docker/platformio/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
+ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
+ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
