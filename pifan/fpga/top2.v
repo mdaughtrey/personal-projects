4a54f3f46b8f31fedc8ccd14f06f9638d3d36c6e
mdaughtrey <mdaughtrey@gmail.com>
2020-01-17 23:25:44 -0500

    pifan

--- pifan/fpga/top2.v
+++ pifan/fpga/top2.v
@@ -12,7 +12,7 @@ module fpga_top(
     input wire SPI0_CLK,
     input wire SPI0_MOSI,
     input wire SPI0_CS,
-    output wire SPI0_MISO,
+    output reg SPI0_MISO,
 	output wire CLK_7SEG,
 	output wire DOUT_7SEG,
 	output wire LOAD_7SEG,
@@ -25,26 +25,53 @@ module fpga_top(
 wire[7:0] spiRx;
 wire[0:0] spiRxReady;
 
+wire[3:0] wdigit0;
+wire[3:0] wdigit1;
+wire[3:0] wdigit2;
+wire[3:0] wdigit3;
+
 MySpi spi(
    // Control/Data Signals,
-   .iClk(WF_CLK),      
+   .sysclk(WF_CLK),      
    .oRxReady(spiRxReady),
    .oRx(spiRx),
 
    // SPI Interface
    .iSPIClk(SPI0_CLK),
    .iSPIMOSI(SPI0_MOSI),
-   .iSPICS(SPI0_CLK)
+   .iSPICS(SPI0_CS),
+   .digit0(wdigit0),
+   .digit1(wdigit1),
+   .digit2(wdigit2),
+   .digit3(wdigit3)
    );
 
-always @(posedge spiRxReady)
-begin
-    digit0 <= digit0 + 1;
-end
+always @(wdigit0) digit0 <= wdigit0;
+always @(wdigit1) digit1 <= wdigit1;
+always @(wdigit2) digit2 <= wdigit2;
+always @(wdigit3) digit3 <= wdigit3;
+//
+//always @(posedge spiRxReady)
+//begin
+//    if (digit0 == 15)
+//    begin
+//    	digit1 <= digit1 + 1;
+//    end
+//	digit0 <= digit0 + 1;
+//end
+
+// DIGITS 3 2 1 0
 
 // -----------------------------------------------------------------------------
 // 7 segment display
 // -----------------------------------------------------------------------------
+initial begin
+    digit0 <= 0;
+    digit1 <= 0;
+    digit2 <= 0;
+    digit3 <= 0;
+end
+
 wire [0:0] two_ms;
 wire [0:0] one_s;
 
@@ -60,21 +87,21 @@ WF_timer #(.COUNT(500), .EVERY_CLK(1'b1)) one_sec(
            .enable(1'b1),
            .timer_pulse(one_s));
 
-always @(posedge one_s)
-begin
-    if (digit2 == 15)
-    begin
-    	digit3 <= digit3 + 1;
-    end
-	digit2 <= digit2 + 1;
-end
+//always @(posedge one_s)
+//begin
+//    if (digit2 == 15)
+//    begin
+//    	digit3 <= digit3 + 1;
+//    end
+//	digit2 <= digit2 + 1;
+//end
 
 
 
 reg [3:0] digit0;    // BCD digits, 0 is LSD
 reg [3:0] digit1;
-reg [3:0] digit2 = 3;
-reg [3:0] digit3 = 4;
+reg [3:0] digit2;
+reg [3:0] digit3;
 reg [1:0] colon = 2'b00;   // 00 colon, 01 decpoint, 11 none
 
 WF_BL_7seg_if seg7(
