56bc2fb7cabdbcbce4e91e1d2ef397122dc25ded
mdaughtrey <mdaughtrey@gmail.com>
2020-01-17 23:25:44 -0500

    pifan

--- pifan/fpga/myspi.v
+++ pifan/fpga/myspi.v
@@ -1,8 +1,8 @@
-
+// Code your design here
 module MySpi
   (
    // Control/Data Signals,
-   input wire  iClk,      // FPGA Clock
+   input wire  sysclk,      // FPGA Clock
    output reg oRxReady,    // Data Valid pulse (1 clock cycle)
    output reg [7:0] oRx,  // Byte received on MOSI
 //   input            iTxReady,    // Data Valid pulse to register i_TX_Byte
@@ -13,31 +13,44 @@ module MySpi
    //output reg o_SPI_MISO, // External pin 21 (48)
 //   output     oSPIMISO, // External pin 21 (48)
    input wire iSPIMOSI, // External pin 20 (45)
-   input wire iSPICS // External pin 19 (47)
+   input wire iSPICS, // External pin 19 (47)
+   output reg[3:0] digit0,
+   output reg[3:0] digit1,
+   output reg[3:0] digit2,
+   output reg[3:0] digit3
    );
 
-reg [2:0] rxBit;
+reg [3:0] rxBit;
 reg [7:0] rxAccum;
+  
+initial begin
+    rxBit <= 0;
+    oRxReady <= 1'b0;
+    rxAccum <= 'h00;
+end
 
-always @(posedge iSPIClk, negedge iSPICS)
+always @(posedge sysclk)
+begin
+    digit0 <= rxAccum[3:0];
+    digit1 <= rxAccum[7:4];
+    digit2 <= rxBit;
+end
+
+always @(rxBit)
+begin
+    rxAccum <= {rxAccum[6:0], iSPIMOSI};
+//    oRxReady <= rxBit[3];
+//    if (rxBit[3]) begin
+//        oRx <= rxAccum;
+//    end
+end
+
+always @(posedge iSPIClk or posedge iSPICS)
 begin
     if (iSPICS) 
-    begin
-        rxBit <= 3'b0;
-    end else begin
-        if (iSPIClk)
-        begin
-            rxAccum <= {rxAccum[7:1], iSPIMOSI};
-            if (rxBit == 3'b111)
-            begin 
-                oRx <= rxAccum;
-                oRxReady <= 1'b1;
-            end else begin
-                oRxReady <= 1'b0;
-            end
-            rxBit <= rxBit + 1;
-        end
-    end
+        rxBit <= 0;
+    else
+        rxBit <= rxBit + 1;
 end
 
 endmodule // MySpi
