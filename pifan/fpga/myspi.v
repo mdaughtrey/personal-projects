b200a574712b5af97ea1c28456c9d52234068e75
mdaughtrey <mdaughtrey@gmail.com>
2020-02-18 23:04:30 -0500

    declare wires/regs

--- pifan/fpga/myspi.v
+++ pifan/fpga/myspi.v
@@ -5,16 +5,16 @@ module MySpi
   (
    // Control/Data Signals,
    input sysclk,      // FPGA Clock
-   output oRxReady,    // Data Valid pulse (1 clock cycle)
-   output [7:0] oRx,  // Byte received on MOSI
+   output wire oRxReady,    // Data Valid pulse (1 clock cycle)
+   output wire [7:0] oRx,  // Byte received on MOSI
    input txReady,
-    input [7:0] tx,
+   input [7:0] tx,
    // SPI Interface
    input iSPIClk,  // External pin 22 (2)
    input iSPIMOSI, // External pin 20 (45)
-   output oSPIMISO, // 
+   output wire oSPIMISO, // 
    input iSPICS, // External pin 19 (47)
-    output [15:0] probe
+   output wire [15:0] probe
    );
 
 reg [2:0] rxBit;
@@ -34,57 +34,65 @@ assign oRxReady = rxReady;
 assign oRx = rxFinal;
 assign oSPIMISO = spiMiso;
   
-// Rx
 always @(posedge iSPIClk or negedge iSPIClk or posedge iSPICS)
 begin
-    if (iSPICS)
-    begin
-        rxBit <= 0;
-        rxReady <= 0;
-        misoState <= 0;
-        txIndex <= 0;
-        spiMiso <= 0;
-        txShift <= 0;
-    end else begin
-        // Rx handler
-        if (iSPIClk) begin
-            rxAccum <= {rxAccum[6:0], iSPIMOSI};
-            if (rxBit == 3'b111) begin
-                rxFinal <= {rxAccum[6:0], iSPIMOSI};
-                rxReady <= 1'b1;
-            end
-            else if (rxBit == 3'b010) begin
-                rxReady <= 1'b0;
-            end
-            // Tx handler
-            case (misoState)
-            // Idle and waiting for txready
-            0: if (txReady) begin
-                txShift <= tx;
-                misoState <= 1;
-            end
-            1: if (!rxBit) begin
-              //  spiMiso <= txShift[7];
-                txIndex <= 6;
-                misoState <= 2;
-            end
-            // Sending bits
-            default: begin
-                //spiMiso <= txShift[txIndex];
-                misoState <= misoState + 1;
-            end
-            // Last bit sent
-            9: misoState <= 0;
-            endcase
-    //      spiMiso <= txShift[0];
-    //      txIndex <= 1;
-    ////      txShift <= txShift >> 1;
-        end
-        else begin
-            rxBit <= rxBit + 1;
-           //if (txIndex) txIndex <= txIndex - 1;
-        end
+    if (iSPICS) spiMiso <= 0;
+    else begin
+        spiMiso <= !spiMiso;
+        rxBit <= rxBit + 1;
     end
 end
+// Rx
+//always @(posedge iSPIClk or negedge iSPIClk or posedge iSPICS)
+//begin
+//    if (iSPICS)
+//    begin
+//        rxBit <= 0;
+//        rxReady <= 0;
+//        misoState <= 0;
+//        txIndex <= 0;
+//        spiMiso <= 0;
+//        txShift <= 0;
+//    end else begin
+//        // Rx handler
+//        if (iSPIClk) begin
+//            rxAccum <= {rxAccum[6:0], iSPIMOSI};
+//            if (rxBit == 3'b111) begin
+//                rxFinal <= {rxAccum[6:0], iSPIMOSI};
+//                rxReady <= 1'b1;
+//            end
+//            else if (rxBit == 3'b010) begin
+//                rxReady <= 1'b0;
+//            end
+//            // Tx handler
+//            case (misoState)
+//            // Idle and waiting for txready
+//            0: if (txReady) begin
+//                txShift <= tx;
+//                misoState <= 1;
+//            end
+//            1: if (!rxBit) begin
+//              //  spiMiso <= txShift[7];
+//                txIndex <= 6;
+//                misoState <= 2;
+//            end
+//            // Sending bits
+//            default: begin
+//                //spiMiso <= txShift[txIndex];
+//                misoState <= misoState + 1;
+//            end
+//            // Last bit sent
+//            9: misoState <= 0;
+//            endcase
+//    //      spiMiso <= txShift[0];
+//    //      txIndex <= 1;
+//    ////      txShift <= txShift >> 1;
+//        end
+//        else begin
+//            rxBit <= rxBit + 1;
+//           //if (txIndex) txIndex <= txIndex - 1;
+//        end
+//    end
+//end
 
 endmodule 
