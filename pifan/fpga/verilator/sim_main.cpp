46c4f3126f77406b228065b65c01f3f5425926b6
mdaughtrey <matt@daughtrey.com>
2020-03-28 17:10:19 -0400

    sim_main.cpp

--- pifan/fpga/verilator/sim_main.cpp
+++ pifan/fpga/verilator/sim_main.cpp
@@ -1,6 +1,8 @@
 #include "Vmyspi.h"
 #include "verilated.h"
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 // VL_IN8(sysclk,0,0);
 // VL_IN8(txReady,0,0);
@@ -22,19 +24,52 @@ double sc_time_stamp()
 class SpiSender {
 public:
     SpiSender(unsigned char);
-    void eval(Vmyspi *, double);
+    void eval(Vmyspi &, double);
 private:
+    const std::vector<int> clkHigh = { 40, 80, 120, 160, 200, 240, 280, 320 };
+    const std::vector<int> clkLow = { 60, 100, 140, 180, 220, 260, 300, 340 };
     int state;
+    int sendbit;
     unsigned char tosend;
 };
 
 SpiSender::SpiSender(unsigned char _tosend)
 {
     this->tosend = tosend;
+    this->sendbit = 7;
+    this->state = 0;
 }
 
-void SpiSender::eval(Vmyspi * top, double main_time)
+void SpiSender::eval(Vmyspi & top, double main_time)
 {
+    if (0 == state)
+    {
+        top.iSPICS = 1;
+        top.iSPIClk = 0;
+    } else if (state == this->clkHigh.front() - 20)
+    {
+        top.iSPICs = 0;
+    } else if (clkHigh.end() !=
+        std::find(this->clkHigh.begin(), this->clkHigh.end(), state))
+    {
+        top.iSPIMOSI = (this->tosend >> sendbit) & 0x01;
+        top.iSPICLK = 1;
+        this->tosend--;
+    } else if (clkLow.end() !=
+        std::find(this->clkLow.begin(), this->clkLow.end(), state))
+    {
+        top.iSPICLK = 0;
+        this.tosend--;
+    } else if (state == this->clkLow.back() + 20)
+    {
+        top.iSPICs = 1;
+    }
+    if (this->state++ > this->clkLow.back() + 40)
+    {
+        return false;
+    }
+    this->state %= this->clkLow.back() + 40;
+    return true;
 }
 
 int main(int argc, char** argv, char** env) {
