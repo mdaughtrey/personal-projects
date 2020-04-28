65113f642e4fd2fe91314d495b9f75fad5fec36b
mdaughtrey <matt@daughtrey.com>
2020-04-27 20:24:27 -0400

    tension

---
+++ projector/scripts/tension.py
@@ -0,0 +1,21 @@
+torque dia
+
+
+unit rotation is proportional to unit length * diameter
+
+R ~ L * D
+D ~ R/L
+
+Cmin = pi*Dmin
+Cmax = pi*Dmax
+
+
+
+
+Dmin 1250
+Dmax 2550
+
+Fmax=10 
+F=Fmax*D/Dmax
+
+
