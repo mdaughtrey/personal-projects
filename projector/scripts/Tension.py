678d1c193469d95644cc317c9f89badbabcb6ace
mdaughtrey <mdaughtrey@gmail.com>
2020-04-28 22:19:04 -0400

    tension

--- projector/scripts/Tension.py
+++ projector/scripts/Tension.py
@@ -10,22 +10,40 @@ class Tension:
     FRAMESPACE_8MM = 3.3
     FRAMESPACE_S8 = 4.01
 
-    def do(self, startdia, enddia):
+    def feedstats(self, startdia, enddia):
         (feedTurns, filmLength) = self.calcLength(enddia, startdia, Tension.FILM_THICKNESS)
         numFrames = math.floor(filmLength / Tension.FRAMESPACE_8MM)
         feedC = self.calcCircums(enddia, math.floor(feedTurns), Tension.FILM_THICKNESS)
         feedD = self.calcDia(enddia, math.floor(feedTurns), Tension.FILM_THICKNESS)
-        tPerTurn = (Tension.TENSION_MAX - Tension.TENSION_MIN) / feedTurns * startdia / Tension.DIA_MAX
+        #tPerTurn = (Tension.TENSION_MAX - Tension.TENSION_MIN) / feedTurns * startdia / Tension.DIA_MAX
         #tensionPerC = [math.floor(Tension.TENSION_MIN + tPerTurn * t) for t in range(math.floor(feedTurns))][::-1]
         #tensionPerC = [Tension.TENSION_MIN + math.tanh((t/2)/(enddia/2)) * (Tension.TENSION_MAX - Tension.TENSION_MIN)  for t in feedD]
         #tensionPerC = [Tension.TENSION_MIN + math.tanh((t/2)/(enddia/2))  for t in feedD]
         ratios = [(d/2)/(enddia/2) for d in feedD]
-        normalized = [self.rescale(r, ratios, range(-3, 4)) for r in ratios]
+        normalized = [self.rescale(r, ratios, range(-2, 2)) for r in ratios]
         tensions = [math.tanh(n) for n in normalized]
         t2 = [math.floor(self.rescale(t, tensions, range(Tension.TENSION_MIN, math.floor(startdia/Tension.DIA_MAX*Tension.TENSION_MAX)))) for t in tensions]
         framesPerC = [math.floor(x/Tension.FRAMESPACE_8MM) for x in feedC][::-1]
         return (filmLength, numFrames, [t2[self.getindex(frame, framesPerC)] for frame in range(numFrames)])
         #return (filmLength, numFrames, [tensionPerC[self.getindex(frame, framesPerC)] for frame in range(numFrames)])
+
+#    def uptakestats(self, length):
+#        pdb.set_trace()
+#        (turns, enddia) = self.calcEndDia(length)
+#        feedD = self.calcDia(Tension.DIA0_UPTAKE, math.floor(turns), Tension.FILM_THICKNESS)[::-1]
+#        ratios = [(d/2)/(Tension.DIA0_UPTAKE/2) for d in feedD]
+#        normalized = [self.rescale(r, ratios, range(-3, 4)) for r in ratios]
+#        tensions = [math.tanh(n) for n in normalized]
+#        t2 = [math.floor(self.rescale(t, tensions, range(Tension.TENSION_MIN, math.floor(startdia/Tension.DIA_MAX*Tension.TENSION_MAX)))) for t in tensions]
+        #return (filmLength, numFrames, [t2[self.getindex(frame, framesPerC)] for frame in range(numFrames)])
+        #(feedTurns, filmLength) = self.calcLength(enddia, startdia, Tension.FILM_THICKNESS)
+        #feedD = self.calcDia(enddia, math.floor(feedTurns), Tension.FILM_THICKNESS)
+#        return (t2, enddia)
+
+#    def calcEndDia(self, length):
+#        turns = self.calcturns(Tension.DIA0_UPTAKE, length, Tension.FILM_THICKNESS)
+#        enddia = 2 * turns * Tension.FILM_THICKNESS + Tension.DIA0_UPTAKE
+#        return (turns, enddia)
 
     def rescale(self, value, scale, scaleto):
         return ((max(scaleto) - min(scaleto))/(max(scale) - min(scale))) * (value - max(scale)) + max(scaleto)
