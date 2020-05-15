a5c5c28a14f525163f239325d141f77da8e89f75
mdaughtrey <matt@daughtrey.com>
2020-05-14 22:35:37 -0400

    qclock

--- esp8266/qclock/qclock.py
+++ esp8266/qclock/qclock.py
@@ -12,6 +12,7 @@ servo.freq(20)
 
 def setlocaltime():
     t = ntptime.time() - 60 * 60 * 4 # hardcoded -4 hours
+    print("NTP time {}".format(t))
     import machine
     import utime
     tm = utime.localtime(t)
@@ -23,9 +24,11 @@ def wakeup():
     sta_if.active(True)
     sta_if.scan()                             # Scan for available access points
     sta_if.connect("Zooma223", "N0stromo") # Connect to an AP
+    servo.duty(servopos[0])
     while False == sta_if.isconnected():
         print("Waiting to connect")
         utime.sleep(1)
+    servo.duty(servopos[1])
     print("Connected {}".format(sta_if.isconnected()))
 
     setlocaltime()
@@ -33,25 +36,33 @@ def wakeup():
     print ("DOW {}".format(dow))
     sta_if.active(False)
     #sta_if.disconnect()
+#    return servopos[dow]
     servo.duty(servopos[dow])
     print("Servo duty {}".format(servo.duty()))
 
 
 def sweep():
     for ii in servopos:
-        utime.sleep(1.0)
+        utime.sleep(1)
         servo.duty(ii)
-    for ii in servopos[::-1]:
-        utime.sleep(1.0)
-        servo.duty(ii)
+#    for ii in servopos[::-1]:
+#        utime.sleep(1.0)
+#        servo.duty(ii)
 
 def main():
     sweep()
     while True:
         wakeup()
-        print("Sleeping for 7200 secs")
-        machine.deepsleep(7200000)
-        #utime.sleep(20)
+        print("Sleeping")
+        utime.sleep(60*60*3)
+
+#    machine.deepsleep(60000)
+    #machine.deepsleep(7200000)
+#    while True:
+#        pass
+#        utime.sleep(10)
+#        print("Sleeping for 7200 secs")
+#        utime.sleep(5)
 
 
 
