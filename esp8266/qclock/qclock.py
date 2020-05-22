56c9cf5fcee46f15f81b43aca00567efb1479081
mdaughtrey <matt@daughtrey.com>
2020-05-22 13:42:54 -0400

    qclock

--- esp8266/qclock/qclock.py
+++ esp8266/qclock/qclock.py
@@ -1,14 +1,22 @@
 import esp
+import math
+import time
 import utime
 import network
 import ntptime
 import machine
 
+gamma = [0, 0, 1, 2, 4, 6, 9, 13, 16, 21, 26, 31, 37, 44, 51, 58, 66, 74, 84, 93, 103, 114, 125, 136, 148, 161, 174, 188, 202, 217, 232, 248, 264, 281, 298, 316, 334, 353, 372, 392, 412, 433, 455, 477, 499, 522, 545, 569, 594, 619, 644, 670, 697, 724, 752, 780, 808, 837, 867, 897, 928, 959, 991, 1023];
+
 servopos = [43, 37, 31, 25, 19, 11, 49]
 
 #sta_if = network.WLAN(network.STA_IF);
 servo = machine.PWM(machine.Pin(14))
-servo.freq(20)
+
+#smpower = machine.Pin(13, machine.Pin.OUT) # blue
+ledpower = machine.Pin(12, machine.Pin.OUT) # red
+ledPWM = machine.PWM(ledpower)
+ledPWM.freq(500)
 
 def setlocaltime():
     t = ntptime.time() - 60 * 60 * 4 # hardcoded -4 hours
@@ -24,6 +32,7 @@ def wakeup():
     sta_if.active(True)
     sta_if.scan()                             # Scan for available access points
     sta_if.connect("Zooma223", "N0stromo") # Connect to an AP
+    servo.freq(20)
     servo.duty(servopos[0])
     while False == sta_if.isconnected():
         print("Waiting to connect")
@@ -41,20 +50,51 @@ def wakeup():
 #    return servopos[dow]
     servo.duty(servopos[dow])
     print("Servo duty {}".format(servo.duty()))
-    sleepfor = 86500 - (lt[3] * 3600) - (lt[4] * 60) - lt[5]
-    print("Sleeping for {} secs".format(sleepfor))
-    return sleepfor
+#    utime.sleep(2)
+#    sleepfor = 86500 - (lt[3] * 3600) - (lt[4] * 60) - lt[5]
+#    print("Sleeping for {} secs".format(sleepfor))
+#    return sleepfor
 
 
 def sweep():
+    ledPWM.duty(0)
+    servo.freq(20)
     for ii in servopos:
-        utime.sleep(1)
         servo.duty(ii)
+        utime.sleep(1)
 
+def pulse():
+    servo.duty(0)
+    ledPWM.freq(500)
+    ledpower.on()
+    for ii in gamma + gamma[::-1]:
+        ledPWM.duty(ii)
+        utime.sleep(0.01)
+    ledpower.off()
+        
 def main():
+    sweep()
+    pulse()
     while True:
-        utime.sleep(wakeup())
-        #wakeup()
+        wakeup()
+        while True:
+            lt = utime.localtime()
+            togo = 86400 - (lt[3] * 3600) - (lt[4] * 60) - lt[5]
+            print("{} seconds to go".format(togo))
+            if 600 > togo:
+                break
+            utime.sleep(600)
+            pulse()
+
+#        utime.sleep(10)
+#    for ii in range(50):
+#        pulse(ledPWM)
+#        print(utime.localtime()[3])
+#        utime.sleep(60)
+#        if utime.localtime[3] == 0:
+#            wakeup()
+#    while True:
+#        utime.sleep(wakeup())
         #rtc = machine.RTC()
         #rtc.irq(trigger = rtc.ALARM0, wake=machine.DEEPSLEEP)
         #utime.sleep(10)
