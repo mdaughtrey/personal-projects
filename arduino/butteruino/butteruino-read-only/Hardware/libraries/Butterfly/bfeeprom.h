1e7d8c2dd81b6e595bb6a7a90085b2ff45ec691c
mdaughtrey <matt@daughtrey.com>
2020-08-01 16:20:56 -0400

    fixer

--- arduino/butteruino/butteruino-read-only/Hardware/libraries/Butterfly/bfeeprom.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef BF_EEPROM_H_
-#define BF_EEPROM_H_
-
-void LoadEEPROM(char *pBuffer, char num_bytes, unsigned char *EE_START_ADR);
-void StoreEEPROM(char *pBuffer, char num_bytes, unsigned char *EE_START_ADR);
-
-#endif
