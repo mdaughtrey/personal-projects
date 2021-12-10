f50607158bbb41bc07ab48a76d9695b8608cd4f5
mdaughtery <mdaughtrey@gmail.com>
2021-12-10 16:15:13 -0500

    proj

--- projector/scripts/hqcap.sh
+++ projector/scripts/hqcap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-PROJECT=$1
-./hqcap1.py --film=8mm --dev=/mnt/exthd --dir /mnt/exthd/${PROJECT}  --single --picameracont --frames 200 # --raspid ${CHILD}
+for ii in 43 44 45 46 47
+do
+    PROJECT="interval_$ii"
+    sudo ./hqcap1.py  --film=8mm --dev=/mnt/exthd --dir /mnt/exthd/${PROJECT}  --single --picameracont \
+    --frames 200 --frameinterval $ii
+done
+
+
+
 #./hqcap1.py --film=8mm --dev=/dev/mmcblk0p2 --dir /var/tmp/${PROJECT}  --single --picameracont # --raspid ${CHILD}
