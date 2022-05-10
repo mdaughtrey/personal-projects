c5fe02ddcaffe7264fcb5b0b1c7658421f1b1bce
mdaughtrey <matt@daughtrey.com>
2022-05-09 23:52:32 -0400

    dev

--- arduino/gamepad/gamepad.ino
+++ arduino/gamepad/gamepad.ino
@@ -14,6 +14,7 @@
 // https://github.com/NicoHood/HID/wiki/Gamepad-API
 
 #include "HID-Project.h"
+#include <AsyncTimer.h>
 
 typedef struct
 {
@@ -22,11 +23,45 @@ typedef struct
     bool (*fun)();
 }Command;
 
+typedef enum
+{
+    EV_BUTTONCHASER = 1,
+    EV_XAXIS = 2,
+    EV_YAXIS = 4,
+    EV_ZAXIS = 8,
+    EV_RXAXIS = 16,
+    EV_RYAXIS = 32,
+    EV_RZAXIS = 64
+}Event;
+
+uint16_t xAxis = 0;
+uint16_t yAxis = 0;
+uint8_t zAxis = 0;
+
+uint8_t rxAxis = 0;
+uint8_t ryAxis = 0;
+uint8_t rzAxis = 0;
+
+uint8_t debug = 0;
+uint8_t doWrite = 0;
+uint8_t event = 0;
 int16_t param;
 uint8_t lastCommand;
+uint32_t buttonState = 0;
+uint8_t writeReady = 0;
+AsyncTimer at;
+//uat.setTimeout(setFrameReady, 10);
+void setEvent(uint8_t ev} { event |= ev; }
+void resetEvent(uint8_t ev} { event &= ~ev; }
+void isEvent(uint8 ev) { event &= ev; }
+
 
 void verbose(const char * fmt, ...)
 {
+    if (!debug)
+    {
+        return;
+    }
     char buffer[256];
     va_list args;
     va_start(args, fmt);
@@ -49,16 +84,144 @@ bool commandB()
     return 1;
 }
 
+bool buttonChaser()
+{
+    buttonState <<= 1;
+    if (~buttonState)
+    {
+        resetEvent(EV_BUTTONCHASER);
+    }
+    if (isEvent(EV_BUTTONCHASER))
+    {
+        writeReady = 1;
+        Gamepad.buttons(buttonState);
+        verbose("buttonChaser %04x\r", buttonState);
+        at.setTimeout(buttonChaser, 100);
+    }
+}
+
+bool xAxisWipe()
+{
+    xAxis++;
+    if (!xAxis)
+    {
+        resetEvent(EV_BUTTONCHASER);
+    }
+    if (isEvent(EV_XAXIS))
+    {
+        writeReady = 1;
+        Gamepad.xAxis(xAxis);
+        verbose("xAxisWipe %u\r", xAxis);
+        at.setTimeout(xAxisWipe, 10);
+    }
+}
+
+bool yAxisWipe()
+{
+    yAxis++;
+    if (!yAxis)
+    {
+        resetEvent(EV_YAXIS);
+    }
+    if (isEvent(EV_YAXIS))
+    {
+        writeReady = 1;
+        Gamepad.yAxis(yAxis);
+        verbose("yAxisWipe %u\r", yAxis);
+        at.setTimeout(yAxisWipe, 10);
+    }
+}
+
+bool zAxisWipe()
+{
+    zAxis++;
+    if (!zAxis)
+    {
+        resetEvent(EV_ZAXIS);
+    }
+    if (isEvent(EV_ZAXIS))
+    {
+        writeReady = 1;
+        Gamepad.zAxis(zAxis);
+        verbose("zAxisWipe %u\r", zAxis);
+        at.setTimeout(zAxisWipe, 10);
+    }
+}
+
+bool rxAxisWipe()
+{
+    rxAxis++;
+    if (!rxAxis)
+    {
+        resetEvent(EV_RXAXIS);
+    }
+    if (isEvent(EV_RXAXIS))
+    {
+        writeReady = 1;
+        Gamepad.rxAxis(rxAxis);
+        verbose("rxAxisWipe %u\r", rxAxis);
+        at.setTimeout(rxAxisWipe, 10);
+    }
+}
+
+bool ryAxisWipe()
+{
+    ryAxis++;
+    if (ryAxis)
+    {
+        writeReady = 1;
+        Gamepad.ryAxis(ryAxis);
+        verbose("ryAxisWipe %u\r", ryAxis);
+        at.setTimeout(ryAxisWipe, 10);
+    }
+}
+
+bool rzAxisWipe()
+{
+    rzAxis++;
+    if (rzAxis)
+    {
+        writeReady = 1;
+        Gamepad.rzAxis(rzAxis);
+        verbose("rzAxisWipe %u\r", rzAxis);
+        at.setTimeout(rzAxisWipe, 10);
+    }
+}
+
+void doReset()
+{
+    xAxis = 0;
+    yAxis = 0;
+    zAxis = 0;
+    rxAxis = 0;
+    ryAxis = 0;
+    rzAxis = 0;
+    debug = 0;
+    doWrite = 0;
+    event = 0;
+}
+
 Command cmds[] = {
     {'b', "Toggle button (param)", commandB},
+    {'B', "Button Chaser", []() { event |= EV_BUTTONCHASER; return 0;}},
     {'c', "Clear param", [](){ param = 0; return 0; }},
+    {'d', "Debug on", []() { debug = 1; return 0;}},
+    {'D', "Debug off", []() { debug = 0; return 0;}},
+    {'e', "X Axis Wipe", []() { event |= EV_XAXIS; return 0; }},
+    {'f', "Y Axis Wipe", []() { event |= EV_YAXIS; return 0; }},
+    {'g', "Z Axis Wipe", []() { event |= EV_ZAXIS; return 0; }},
     {'h', "Help", [](){ help(); return 0; }},
+    {'i', "RX Axis Wipe", []() { event |= EV_RXAXIS; return 0; }},
+    {'j', "RY Axis Wipe", []() { event |= EV_RYAXIS; return 0; }},
+    {'k', "RZ Axis Wipe", []() { event |= EV_RZAXIS; return 0; }},
+    {'w', "USB Write on", []() { doWrite = 1; return 0; }},
+    {'W', "USB Write off", []() { doWrite = 0; return 0; }},
     {'x', "16-bit X Axis (param)", [](){ Gamepad.xAxis(param); return 1; }},
     {'y', "16-bit Y Axis (param)", [](){ Gamepad.yAxis(param); return 1; }},
     {'z', "8-bit lZ Axis (param)", [](){ Gamepad.zAxis(param); return 1; }},
     {'X', "8-bit X Axis (param)", [](){ Gamepad.rxAxis(param); return 1; }},
     {'Y', "8-bit Y Axis (param)", [](){ Gamepad.ryAxis(param); return 1; }},
-    {' ', "Release all buttons", [](){  Gamepad.releaseAll(); return 1; }}
+    {' ', "Reset", [](){ doReset(); return 0; }}
 };
 
 #define NUMCOMMANDS (sizeof(cmds)/sizeof(cmds[0]))
@@ -82,7 +245,7 @@ void handleCommand()
         {
             if (cmds[ii].fun())
             {
-                Gamepad.write();
+                writeReady = 1;
             }
             return;
         }
@@ -100,8 +263,53 @@ void loop() {
     {
         handleCommand();
     }
-
-
+    if (writeReady)
+    {
+        if (doWrite)
+        {
+            Gamepad.write();
+        }
+        writeReady = 0;
+    }
+    if (event & EV_BUTTONCHASER)
+    {
+        event &= ~EV_BUTTONCHASER;
+        buttonChaser();
+    }
+    if (event & EV_XAXIS)
+    {
+        event &= ~EV_XAXIS;
+        xAxisWipe();
+    }
+    if (event & EV_YAXIS)
+    {
+        event &= ~EV_YAXIS;
+        yAxisWipe();
+    }
+    if (event & EV_ZAXIS)
+    {
+        event &= ~EV_ZAXIS;
+        zAxisWipe();
+    }
+    if (event & EV_RXAXIS)
+    {
+        event &= ~EV_RXAXIS;
+        rxAxisWipe();
+    }
+    if (event & EV_RYAXIS)
+    {
+        event &= ~EV_RYAXIS;
+        ryAxisWipe();
+    }
+    if (event & EV_RZAXIS)
+    {
+        event &= ~EV_RZAXIS;
+        rzAxisWipe();
+    }
+    if (event)
+    {
+        at.handle();
+    }
 
 //  if (!digitalRead(pinButton)) {
 //    digitalWrite(pinLed, HIGH);
