638abac7c23f23402318185893e338dad543108b
mdaughtrey <matt@daughtrey.com>
2022-10-27 22:34:15 -0400

    USB Camera autocrop

--- projector/v4l2capture/util.sh
+++ projector/v4l2capture/util.sh
@@ -6,15 +6,32 @@ SHAREDIR=/home/mattd/share
 #VIDEOSIZE=640x480
 VIDEOSIZE=1280x720
 
+getdev()
+{
+    mapfile devices<<<$(v4l2-ctl --list-devices)
+    for ((ii=0; ii<${#devices[@]}; ii++)); do
+        [[ "${devices[$ii]}" =~ .*SVBONY ]] && echo ${devices[$((ii+1))]} && break
+    done
+}
+
+clip()
+{
+    ./autocrop.py --inputdir ${SHAREDIR} --outputdir ${SHAREDIR}/cropped --serialize
+}
+
 case "$1" in 
     dev) ffmpeg -devices ;;
     caps) ffmpeg -f v4l2 -list_formats all -i ${DEVICE} ;;
+    capbmp) 
+        ffmpeg -f v4l2 -framerate 1 -video_size ${VIDEOSIZE} -i $(getdev) -y ${SHAREDIR}/%08d.bmp ;;
     capmjpeg) 
-        sudo ffmpeg -f v4l2 -framerate 1 -video_size ${VIDEOSIZE} -i ${DEVICE} -y ${SHAREDIR}/capmjpeg_${VIDEOSIZE}.avi ;;
-    copy) sudo ffmpeg -f v4l2 -video_size ${VIDEOSIZE} -i ${DEVICE} -vcodec copy -y ${SHAREDIR}/rawout_${VIDEOSIZE}.avi ;;
-    raw2img) ffmpeg -i ${SHAREDIR}/rawout_${VIDEOSIZE}.avi -vf fps=1 ${SHAREDIR}/frame%d.png ;;
-    pipe) sudo ffmpeg -f v4l2 -video_size ${VIDEOSIZE} -i ${DEVICE} -vf fps=1 ${SHAREDIR}/frame%d.png ;;
+        ffmpeg -f v4l2 -framerate 1 -video_size ${VIDEOSIZE} -i ${DEVICE}  -vf fps=10 -y ${SHAREDIR}/capmjpeg_${VIDEOSIZE}.avi ;;
+    copy) ffmpeg -f v4l2 -video_size ${VIDEOSIZE} -i ${DEVICE} -vcodec copy -y ${SHAREDIR}/rawout_${VIDEOSIZE}.avi ;;
+    raw2img) ffmpeg -i ${SHAREDIR}/rawout_${VIDEOSIZE}.avi -vf fps=10 ${SHAREDIR}/frame%d.png ;;
+    pipe) ffmpeg -f v4l2 -video_size ${VIDEOSIZE} -i ${DEVICE} -vf fps=1 ${SHAREDIR}/frame%d.png ;;
     stream) ffmpeg -video_size 640x480 -rtbufsize 702000k -framerate 10 -i video="${DEVICE}" -threads 4 -vcodec libh264 -crf 0 -preset ultrafast -f mpegts "udp://pop-os:56666" ;;
+    getdev) getdev ;;
+    clip) clip ;;
     *) echo what?
 esac
 
