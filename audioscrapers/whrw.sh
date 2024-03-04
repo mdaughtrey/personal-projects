4da56c6f223e0d2697c1de7861809771b27c95bd
mdaughtrey <matt@daughtrey.com>
2024-03-04 17:27:21 -0500

    whrw

--- audioscrapers/whrw.sh
+++ audioscrapers/whrw.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 OUTFILE=$(echo "DJ_Pokey_$(date).mp3" | sed 's/ /_/g;s/:/_/g')
-echo timeout 100m wget https://securestreams4.autopo.st:1794/hi \ 
-    -o /dev/null \
-    -O /home/mattd/Downloads/"${OUTFILE}" >> /var/tmp/whrw.txt
+timeout 100m wget https://securestreams4.autopo.st:1794/hi -o /dev/null -O /home/mattd/Downloads/"${OUTFILE}" >> /var/tmp/whrw.txt
