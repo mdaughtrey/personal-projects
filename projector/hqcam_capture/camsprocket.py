0b60744f4d4005a86f88f4bc8000434d3932c127
mdaughtrey <matt@daughtrey.com>
2024-03-27 23:14:50 -0400

    dev

--- projector/hqcam_capture/camsprocket.py
+++ projector/hqcam_capture/camsprocket.py
@@ -1,64 +1,93 @@
 #!/bin/env python3
 
 import cv2
+from  glob import glob, iglob
 import numpy as np
 from PIL import Image, ImageOps
 from matplotlib import pyplot as plt
 from scipy import ndimage
 import pdb
+#import cProfile
+from picamera2 import Picamera2, Preview
+from libcamera import Transform
 
-pdb.set_trace()
 #image = cv2.imread('/frames/20240326_640_480/capture/00000000_22000.png')
-image = cv2.imread('/media/frames/frames_for_cam_sprocket/0089.png')
-y,x = image.shape[:2]
-image = image[int(y/4):y-int(y/4),0:int(x/4)]
-plt.imshow(image)
 
-image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image3 = np.asarray(image2, dtype=np.uint8)
-image3 = ndimage.grey_erosion(image3, size=(5,5))
-_, image3 = cv2.threshold(image3, 100, 255, cv2.THRESH_BINARY)
-plt.imshow(image3,cmap='gray')
+def findSprocket(image, show=False):
+    y,x = image.shape[:2]
+    if show:
+        plt.imshow(image)
+        plt.show()
+    image = image[int(y/3):y-int(y/3),0:int(x/4)]
+    if show:
+        plt.imshow(image)
+        plt.show()
+    image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image3 = np.asarray(image2, dtype=np.uint8)
+    image3 = ndimage.grey_erosion(image3, size=(5,5))
+    _, image3 = cv2.threshold(image3, 100, 255, cv2.THRESH_BINARY)
+    if show:
+        plt.imshow(image3,cmap='gray')
+        plt.show()
+
+    def whtest(contour):
+        (x,y,w,h) = cv2.boundingRect(contour)
+        return (50 < w < 60) & (60 < h < 70)
+
+    # Find the contours in the thresholded image
+    contours, _ = cv2.findContours(image3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = list(filter(whtest, contours))
+    if 1 != len(contours):
+        return False
+    if show == False:
+        return True
+
+    for c in contours:
+        print('Contour Area: ' + str(cv2.contourArea(c)))
+        x, y, width, height = cv2.boundingRect(c)
+        print(f'x {x} y {y} width {width} height {height}')
+    contour = contours[0]
+
+    # Get the bounding box of the largest contour
+    x, y, width, height = cv2.boundingRect(contour)
+
+    # Print the size and location of the white square
+    print(f'White square size: {width}x{height} pixels')
+    print(f'White square location: ({x}, {y})')
+    cv2.drawContours(image3, [contour], -1, (100,100,100), thickness=cv2.FILLED)
+    plt.imshow(image3,cmap='gray')
+    plt.show()
+
+    return True
+
+w,h = (640,480)
+#pdb.set_trace()
+#image = cv2.imread('/media/frames/frames_for_cam_sprocket/0877.png')
+#findSprocket(image)
+picam = Picamera2()
+#config = picam.create_video_preview_configuration(main={'size':(640,480)})
+config = picam.create_video_configuration(main={'size': (640,480), "format":"RGB888"},
+                                          transform = Transform(hflip=True),
+                                          lores={"size":(640,480),"format":"RGB888"},
+                                          controls={'FrameDurationLimits':(1000000,1000000),
+                                                    'ExposureTime':22000})
+picam.configure(config)
+picam.align_configuration(config)
+picam.start()
+
+while True:
+#    pdb.set_trace()
+    buffer = picam.capture_array("lores")
+#    cv2.imwrite('test.png', buffer)
+    #buffer = buffer[:w * h].reshape(h,w)
+#    buffer = buffer.reshape(h,w,3)
+    print(str(findSprocket(buffer,show = False)))
+#    picam.stop()
 
 
-#matrix = (3,7)
-#image4 = cv2.GaussianBlur(image3, matrix, 0)
-#plt.imshow(image4,cmap='gray')
-#print(image4)
-# Apply a threshold to the grayscale image to find the white square
-#_, thresh = cv2.threshold(image4, 100, 255, cv2.THRESH_BINARY)
-#plt.imshow(thresh,cmap='gray')
-def whtest(contour):
-    (x,y,w,h) = cv2.boundingRect(contour)
-    return (50 < w < 60) & (60 < h < 70)
-    
+#pdb.set_trace()
+#for image in sorted(glob('/media/frames/frames_for_cam_sprocket/*.png')):
+#    print(image + ' ' + str(findSprocket(cv2.imread(image), show = False)))
+#image = cv2.imread('/media/frames/frames_for_cam_sprocket/0877.png')
+#cProfile.run("findSprocket(image)")
 
-# Find the contours in the thresholded image
-contours, _ = cv2.findContours(image3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours = filter(whtest, contours)
-contour = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)[0]
-
-for c in contours:
-    print('Contour Area: ' + str(cv2.contourArea(c)))
-    x, y, width, height = cv2.boundingRect(c)
-    print(f'x {x} y {y} width {width} height {height}')
-
-# Loop through the contours and find the largest one, which should be the white square
-#largest_area = 0
-#largest_contour = None
-#for contour in contours:
-#    area = cv2.contourArea(contour)
-#    #print(area)
-#    #print(contour)
-#    if area > largest_area:
-#        largest_area = area
-#        largest_contour = contour
-
-# Get the bounding box of the largest contour
-x, y, width, height = cv2.boundingRect(contour)
-
-# Print the size and location of the white square
-print(f'White square size: {width}x{height} pixels')
-print(f'White square location: ({x}, {y})')
-cv2.drawContours(image3, [contour], -1, (100,100,100), thickness=cv2.FILLED)
-plt.imshow(image3,cmap='gray')
