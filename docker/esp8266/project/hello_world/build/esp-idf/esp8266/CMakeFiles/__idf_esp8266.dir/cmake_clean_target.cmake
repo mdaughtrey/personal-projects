d59bf3d4b447a93489e8b163593dbd5cc01e3e1e
mattd <mattd@pop-os.localdomain>
2024-04-09 20:58:07 -0400

    remove docker

--- docker/esp8266/project/hello_world/build/esp-idf/esp8266/CMakeFiles/__idf_esp8266.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libesp8266.a"
-)
--- docker/nikola/demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /personal-projects/docker/nikola
-#nikola init -q --demo daughtrey.com
-cd daughtrey.com
-nikola build
-nikola serve
